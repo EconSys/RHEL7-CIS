5c6dbd01be28653060f59f58f89e2242b4227378
tkhanateconsysdotcom <45574294+tkhanateconsysdotcom@users.noreply.github.com>
2019-05-01 15:40:39 -0400

    Update runsible.sh

--- RHEL7-CIS/runsible.sh
+++ RHEL7-CIS/runsible.sh
@@ -4,12 +4,6 @@ set -o errexit
 
 install_dependencies () {
 sudo yum -y update
-
-local_co=""
-if [ -z "$IP" ]; then
-  IP="127.0.0.1"
-  local_co="-c local"
-fi
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y --enablerepo=extras install epel-release
 sudo yum install -y python-pip git python-dev
@@ -19,6 +13,13 @@ cd RHEL7-CIS && cd RHEL7-CIS
 }
 
 run_playbook () {
+
+local_co=""
+if [ -z "$IP" ]; then
+  IP="127.0.0.1"
+  local_co="-c local"
+fi
+
 ansible-playbook -b -u $USER $local_co -i "$IP," playbook.yml
 }
 
