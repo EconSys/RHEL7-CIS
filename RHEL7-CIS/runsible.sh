bf07fb5f949bb97d7b1cd4629e305454efcc2e33
tkhanateconsysdotcom <45574294+tkhanateconsysdotcom@users.noreply.github.com>
2019-05-02 17:07:28 -0400

    Update runsible.sh

--- RHEL7-CIS/runsible.sh
+++ RHEL7-CIS/runsible.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-set -o errexit
-
-install_dependencies () {
-{ sudo yum -y update } ||
-{ sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm } ||
-{ sudo yum -y --enablerepo=extras install epel-release } ||
-{ sudo yum install -y python-pip git python-dev } ||
-{ sudo pip install ansible markupsafe } ||
-{ git clone https://github.com/EconSys/RHEL7-CIS.git } ||
-{ cd RHEL7-CIS && cd RHEL7-CIS }
-}
-
-run_playbook () {
-
+sudo yum -y update
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y --enablerepo=extras install epel-release
+sudo yum install -y python-pip git python-dev
+sudo pip install ansible markupsafe
+git clone https://github.com/EconSys/RHEL7-CIS.git
+cd RHEL7-CIS && cd RHEL7-CIS
 local_co=""
 if [ -z "$IP" ]; then
   IP="127.0.0.1"
@@ -22,7 +15,3 @@ fi
 
 ansible-playbook -b -u $USER $local_co -i "$IP," playbook.yml
 service auditd restart
-}
-
-install_dependencies
-run_playbook
